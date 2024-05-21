37e2b793d708137e3ab844657a833664a6804a7f
VanHoang1504 <hoangcvph42448@fpt.edu.vn>
2024-05-21 12:07:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab4ph42448kotlin/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.lab4ph42448kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab4ph42448kotlin.ui.theme.Lab4Ph42448kotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab4Ph42448kotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab4Ph42448kotlinTheme {
+        Greeting("Android")
+    }
+}
